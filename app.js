56cef025d93f9529e56fe095c0a42124d5ec6254
amit15000 <135760038+amit15000@users.noreply.github.com>
2023-06-07 16:20:24 +0530

    api change

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const url = "https://us21.api.mailchimp.com/3.0/lists/06b8cecbb9"
 
 const options = {
   method  : "Post",
-  auth : "amit15000:b64d04e43a6b5fe5ae388bcd61decc44-us21"
+  auth : "amit15000:ca1a22837c5a975ca9574a4aba38c062-us21"
 }
 
 const request = https.request(url, options, function(response){
